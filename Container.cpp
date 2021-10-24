0594198cb345589d06f3e8ba9fcb68cea2bb14ca
Valov T <71039576+timofius3@users.noreply.github.com>
2021-10-24 20:58:56 +0000

    added tests

--- Container.cpp
+++ Container.cpp
@@ -51,23 +51,20 @@ void Container::merge(int const left, int const mid, int const right)
     auto const subArrayOne = mid - left + 1;
     auto const subArrayTwo = right - mid;
   
-    // Create temp arrays
     Matrix *leftArray [subArrayOne];
     Matrix *rightArray [subArrayTwo];
   
-    // Copy data to temp arrays leftArray[] and rightArray[]
     for (auto i = 0; i < subArrayOne; i++)
         leftArray[i] = container_[left + i];
     for (auto j = 0; j < subArrayTwo; j++)
         rightArray[j] = container_[mid + 1 + j];
   
-    auto indexOfSubArrayOne = 0, // Initial index of first sub-array
-        indexOfSubArrayTwo = 0; // Initial index of second sub-array
-    int indexOfMergedArray = left; // Initial index of merged array
-  
-    // Merge the temp arrays back into array[left..right]
+    auto indexOfSubArrayOne = 0, 
+        indexOfSubArrayTwo = 0; 
+    int indexOfMergedArray = left; 
+
     while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
-        if (leftArray[indexOfSubArrayOne]->getAverage() <= rightArray[indexOfSubArrayTwo]->getAverage()) {
+        if (leftArray[indexOfSubArrayOne]->getAverage() >= rightArray[indexOfSubArrayTwo]->getAverage()) {
             container_[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
             indexOfSubArrayOne++;
         }
@@ -77,15 +74,13 @@ void Container::merge(int const left, int const mid, int const right)
         }
         indexOfMergedArray++;
     }
-    // Copy the remaining elements of
-    // left[], if there are any
+
     while (indexOfSubArrayOne < subArrayOne) {
         container_[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
         indexOfSubArrayOne++;
         indexOfMergedArray++;
     }
-    // Copy the remaining elements of
-    // right[], if there are any
+
     while (indexOfSubArrayTwo < subArrayTwo) {
         container_[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
         indexOfSubArrayTwo++;
